ba45f7de1d65eefe9e0823bbe44bba5056b58e50
JuanSuppicich <suppicichjuan@gmail.com>
2021-07-24 17:47:34 -0300

    Add eureka server project

---
+++ src/test/java/com/durandsuppicich/daneureka/DanEurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.durandsuppicich.daneureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class DanEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
